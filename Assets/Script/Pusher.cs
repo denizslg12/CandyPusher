d0a18f87cbe901b9c785f03b99179d697ab6eca8
denizslg12 <deniznarutosalgin2003@gmail.com>
2026-05-07 12:21:52 +0900

    unity

---
+++ Assets/Script/Pusher.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    public float speed = 1f;
+    public float movePower = 5f;
+    private Vector3 startPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = this.transform.position;
+        Debug.Log("ÉQÅ[ÉÄÇ™äJénÇµÇΩÇÊ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
